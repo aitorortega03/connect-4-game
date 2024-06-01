702a883963b66a7bee3df9d91a41e0d58bff8287
aitorortega03 <aitorortega21@gmail.com>
2024-06-01 16:24:02 +0200

    Done turn change on click

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,27 @@ const TURNS = {
   O: 'o'
 }
 
-const Square = ({ children, updateBoard, index }) => {
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+  const handleClick = () => {
+    updateBoard()
+  }
   return (
-    <div className='square'>
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
 
 function App() {
-  const [board, setBoard] = useState(['x','x','x','x','x','x','x','x','x',])
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+
+  const updateBoard = () => {
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+  }
   return (
       <main className="board">
         <h1>Connect 4 Game</h1>
@@ -26,12 +37,17 @@ function App() {
                 <Square
                   key={index}
                   index={index}
+                  updateBoard={updateBoard}
                 >
                   {board[index]}
                 </Square>
               )
             })
           }
+        </section>
+        <section className='turn'>
+          <Square isSelected={turn === TURNS.X} >{TURNS.X}</Square>
+          <Square isSelected={turn === TURNS.O} >{TURNS.O}</Square>
         </section>
       </main>
   );
